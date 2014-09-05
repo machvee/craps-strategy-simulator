6bd55e9cba105b0fd3e48065c90def071e289312
Dave McVicar <dj@mcvicar.com>
2014-09-05 11:26:31 -0400

    use the new point? helper method in table_state

--- lib/strategies/prompt_strategy.rb
+++ lib/strategies/prompt_strategy.rb
@@ -5,7 +5,7 @@ class PromptStrategy < BaseStrategy
     player.pass_line_bet if table_state.off? unless player.has_bet?(PassLineBet)
     player.pass_odds_bet if player.has_bet?(PassLineBet) unless player.has_bet?(PassOddsBet, table_state.point)
     [4,6,8,10].each do |n|
-      player.hardways_bet(n, 5) if table_state.point == n unless player.has_bet?(HardwaysBet, n)
+      player.hardways_bet(n, 5) if table_state.point?(n) unless player.has_bet?(HardwaysBet, n)
     end
     [6,8].each do |n|
       player.place_bet(n) unless player.has_bet?(PlaceBet, n) || table_state.point?(n)
