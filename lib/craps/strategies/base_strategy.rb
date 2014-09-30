955b0c03f4312d56c7b85c9c8783df7defa9d41e
David McVicar <dave.mcvicar@livingsocial.com>
2014-09-29 22:36:28 -0400

    refined basic strategy to be more aggressive when the table is HOT

--- lib/craps/strategies/base_strategy.rb
+++ lib/craps/strategies/base_strategy.rb
@@ -19,41 +19,48 @@ class BaseStrategy
   def pass_line_bet_with_full_odds
     player.pass_line if table_state.off?
     player.pass_odds if table_state.on? && player.has_bet?('pass_line_point')
+    player
   end
 
   def hardways_bet_on_the_point(amount)
     player.hardways(table_state.point, amount) if table_state.on? &&
       CrapsDice::HARDS.include?(table_state.point)
+    player
   end
 
   def all_the_hardways(amount=nil)
     CrapsDice::HARDS.each do |n|
       player.hardways(n, amount)
     end
+    player
   end
 
   def six_and_eight
     [6,8].each do |n|
       player.place(n) unless table_state.point?(n)
     end if table_state.on?
+    player
   end
 
   def inside
     CrapsDice::INSIDE.each do |n|
       player.place(n) unless table_state.point?(n)
     end if table_state.on?
+    player
   end
 
   def across
     CrapsDice::POINTS.each do |n|
       player.place(n) unless table_state.point?(n)
     end if table_state.on?
+    player
   end
 
   def all_across
     CrapsDice::POINTS.each do |n|
       player.place(n)
     end if table_state.on?
+    player
   end
 
   def come_out_bet_with_full_odds
@@ -63,20 +70,24 @@ class BaseStrategy
         player.come_odds(number) if player.has_bet?('come', number)
       end
     end
+    player
   end
 
   def all_prop_bets(amount = nil)
     Table::PROPOSITION_BETS.each do |bet|
       player.send(bet.short_name, amount)
     end
+    player
   end
 
   def field
     player.field
+    player
   end
 
   def craps_check
     player.ce
+    player
   end
 
 end
--- lib/craps/strategies/basic_strategy.rb
+++ lib/craps/strategies/basic_strategy.rb
@@ -5,9 +5,16 @@ class BasicStrategy < BaseStrategy
     # make a full passline odds bet
     # if the point is a hardways number, make a $5 hardways bet
     # place $12 bet on 6 and 8 unless either is the point
+    # make full inside place bets if table is hot
+    # TBD: take full winnings on first 6/8 place after the point, but go
+    # up a unit each win after that
     #
     pass_line_bet_with_full_odds
-    six_and_eight
+    if table.hot_or_cold == 'HOT'
+      inside
+    else
+      six_and_eight 
+    end
     hardways_bet_on_the_point(5)
   end
 
