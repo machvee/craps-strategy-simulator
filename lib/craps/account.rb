0ddd25640eb3f01fb6908586262bb498c11482a9
Dave McVicar <dj@mcvicar.com>
2014-10-01 11:14:57 -0400

    Create account.rb

---
+++ lib/craps/account.rb
@@ -0,0 +1,7 @@
+class Account
+  #
+  # maintains money.  allows credit, debit, transfer between accounts
+  # and maintains active money, reserves, and markers.  Both House and
+  # players should be able to use this, at least as a base class
+  #
+end
