9640d67a4d8134328003e33881962a9a17380f14
David McVicar <dave.mcvicar@livingsocial.com>
2014-11-23 18:21:55 -0500

    complete spec tests for TableState

--- spec/lib/table_state_spec.rb
+++ spec/lib/table_state_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 describe TableState do
   before(:each) do
     @dice = double("dice")
-    @table = double("table", dice: @dice)
+    @shooter = double("shooter", done: nil)
+    @table = double("table", dice: @dice, shooter: @shooter)
     @history_length = 5
   end
 
@@ -150,5 +151,102 @@ describe TableState do
       value: 4
     )
     expect(@table_state.point_made?(5)).to be false
+  end
+
+  it 'should respond true to front_line_winner? when a winner rolled and table is off' do
+    @table_state = TableState.new(@table, @history_length)
+    @table_state.table_off
+    allow(@dice).to receive_messages(
+      winner?: true
+    )
+    expect(@table_state.front_line_winner?).to be true
+  end
+
+  it 'should respond false to front_line_winner? when a non-winner rolled and table is off' do
+    @table_state = TableState.new(@table, @history_length)
+    @table_state.table_off
+    allow(@dice).to receive_messages(
+      winner?: false
+    )
+    expect(@table_state.front_line_winner?).to be false
+  end
+
+  it 'should respond true to front_line_winner?(7) when a 7 rolled and table is off' do
+    @table_state = TableState.new(@table, @history_length)
+    @table_state.table_off
+    allow(@dice).to receive_messages(
+      value: 7,
+      winner?: true
+    )
+    expect(@table_state.front_line_winner?(7)).to be true
+    expect(@table_state.winner_seven?).to be true
+  end
+
+  it 'should respond false to front_line_winner?(7) when a 11 rolled and table is off' do
+    @table_state = TableState.new(@table, @history_length)
+    @table_state.table_off
+    allow(@dice).to receive_messages(
+      value: 11,
+      winner?: true
+    )
+    expect(@table_state.front_line_winner?(7)).to be false
+    expect(@table_state.yo_eleven?).to be true
+  end
+
+  it 'should respond true to crapped_out? when a craps rolled and table is off' do
+    @table_state = TableState.new(@table, @history_length)
+    @table_state.table_off
+    allow(@dice).to receive_messages(
+      craps?: true
+    )
+    expect(@table_state.crapped_out?).to be true
+  end
+
+  it 'should respond false to crapped_out? when a non-craps rolled and table is off' do
+    @table_state = TableState.new(@table, @history_length)
+    @table_state.table_off
+    allow(@dice).to receive_messages(
+      craps?: false
+    )
+    expect(@table_state.crapped_out?).to be false
+  end
+
+  it 'should update the table state to on given the table off and point number rolled then off when rolled again' do
+    roll_counter = instance_double('FrequencyCounter')
+    expect(roll_counter).to receive(:reset).with(no_args)
+    expect(roll_counter).to receive(:commit).with(no_args)
+    @table_state = TableState.new(@table, @history_length, frequency_counter: roll_counter)
+    allow(@dice).to receive_messages(
+      value: 9,
+      points?: true
+    )
+    @table_state.update
+    expect(@table_state.on?).to be true
+    expect(@table_state.point).to equal(9)
+    @table_state.update
+    expect(@table_state.off?).to be true
+    expect(@table_state.point).to equal(nil)
+  end
+
+  it 'should set table off and invoke table callbacks when seven out' do
+    roll_counter = instance_double('FrequencyCounter')
+    expect(roll_counter).to receive(:commit).with(no_args)
+    expect(roll_counter).to receive(:reset).with(no_args)
+    expect(@shooter).to receive(:done).with(no_args)
+    expect(@table).to receive(:reset_player_strategies).with(no_args)
+
+    @table_state = TableState.new(@table, @history_length, frequency_counter: roll_counter)
+    @table_state.table_on_with_point(4)
+    expect(@table_state.on?).to be true
+    expect(@table_state.point).to equal(4)
+
+    allow(@dice).to receive_messages(
+      seven?: true,
+      value: 7,
+      points?: false
+    )
+    @table_state.update
+    expect(@table_state.off?).to be true
+    expect(@table_state.point).to equal(nil)
   end
 end
