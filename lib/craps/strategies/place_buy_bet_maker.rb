e18d97f34ec4397f0f970b99d1f3a4d419cef5e9
David McVicar <dave.mcvicar@livingsocial.com>
2014-12-01 22:56:50 -0500

    cleaned up to ensure nil returned when no place bet is made

--- lib/craps/strategies/place_buy_bet_maker.rb
+++ lib/craps/strategies/place_buy_bet_maker.rb
@@ -21,9 +21,10 @@ class PlaceBuyBetMaker < BetMaker
     #   a. no place/buy/pass_line_point/come bet exists in the box
     #   b. no place/buy but there is a come/pass_line and bets_working
     #
-    if no_place_or_buy && (no_passline_or_come || bets_working)
+    bet = if no_place_or_buy && (no_passline_or_come || bets_working)
       put_down_the_bet
     end
+    bet
   end
 
   def ensure_bet_to_possibly_bump_bet_amount(place_or_buy_present, no_passline_or_come)
@@ -31,15 +32,18 @@ class PlaceBuyBetMaker < BetMaker
     # ensure the bet in place if:
     #   a place/buy bet exists AND NO pass_line_point/come bet exists in the box
     #
-    if place_or_buy_present && no_passline_or_come
-      bet = player.ensure_bet(bet_short_name, bet_presser.next_bet_amount, number)
-      bet.maker = self
+    bet = if place_or_buy_present && no_passline_or_come
+      player.ensure_bet(bet_short_name, bet_presser.next_bet_amount, number).tap do |b|
+        b.maker = self
+      end
     end
+    bet
   end
 
   def put_down_the_bet
-    bet = player.make_bet(bet_short_name, bet_presser.next_bet_amount, number)
-    bet.maker = self
+    player.make_bet(bet_short_name, bet_presser.next_bet_amount, number).tap do |bet|
+      bet.maker = self
+    end
   end
 
   def there_is_no_existing_pass_line_point_or_come_bet
